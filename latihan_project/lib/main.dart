1b06ac888934ce9fa4141659004aebc068276647
unknown <elliciaserliyaw@gmail.com>
2022-12-06 11:25:55 +0700

    pertemuan 10

--- latihan_project/lib/main.dart
+++ latihan_project/lib/main.dart
@@ -1,51 +1,54 @@
-import 'package:flutter/material.dart';
-import 'pages/myactionbutton.dart';
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Project UI',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ProjectPage (),
-    );
-  }
-}
-//klik stls , lalu enter maka muncul seperti baris 24-31
-class ProjectPage extends StatelessWidget {
-  const ProjectPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: MyActionButton(),
-      floatingActionButtonLocation: 
-      floatingActionButtonLocation.centerDocked,
-      appBar: MyAppBar(),
-      body: Container(
-        child: ListView(
-          children: [
-            SizedBox(height: 16.0,),
-            MyAppBar(),
-            SizedBox(height: 16.0,),
-            FoodListView(),
-            SizedBox(height: 16.0,),
-            SelectTypeSection(),
-            SizedBox(height: 16.0,),
-            MenuItemList(),
-
-          ],
-        ),
-      ),
-      );
-  }
+import 'package:flutter/material.dart'; 
+import 'pages/myactionbutton.dart'; 
+import 'pages/mybuttombar.dart'; 
+import 'pages/foodlistview.dart'; 
+import 'pages/selecttypesection.dart'; 
+import 'pages/menuitemlist.dart'; 
+import 'pages/myappbar.dart'; 
+ 
+void main() { 
+  runApp(const MyApp()); 
+} 
+ 
+class MyApp extends StatelessWidget { 
+  const MyApp({Key? key}) : super(key: key); 
+ 
+  // This widget is the root of your application. 
+  @override 
+  Widget build(BuildContext context) { 
+    return MaterialApp( 
+      title: 'Project UI', 
+      debugShowCheckedModeBanner: false, 
+      theme: ThemeData( 
+        primarySwatch: Colors.blue, 
+      ), 
+      home: ProjectPage(), 
+    ); 
+  } 
+} 
+ 
+class ProjectPage extends StatelessWidget { 
+  @override 
+  Widget build(BuildContext context) { 
+    return Scaffold( 
+      floatingActionButton: MyActionButton(), 
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, 
+      bottomNavigationBar: MyBottomBar(), 
+      body: Container( 
+        child: ListView( 
+          children: [ 
+            SizedBox(height: 16.0,), 
+            MyAppBar(), 
+            SizedBox(height: 16.0,), 
+            FoodListView(), 
+            SizedBox(height: 16.0,), 
+            SelectTypeSection(), 
+            SizedBox(height: 16.0,), 
+            MenuItemsList(), 
+          ], 
+        ), 
+      ), 
+       
+    ); 
+  } 
 }
